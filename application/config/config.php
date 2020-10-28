e932c200fa5f09e4f08810e83065eeea712fe0ed
cendydwierianto <cendydwie47@gmail.com>
2020-10-28 14:25:55 +0700

    Project Web

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['app_name'] = 'Project_UAS';
+$config['app_name'] = 'Project_Web';
 
 $config['base_url'] = 'http://localhost/project_web';
 
